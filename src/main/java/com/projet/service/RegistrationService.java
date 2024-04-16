a609fd27f9242660853dd2371e4077b3a0d59aec
Seddiki Ossama <seddiki915@gmail.com>
2024-04-16 08:36:30 +0100

    bug fixes

--- src/main/java/com/projet/service/RegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.projet.service;
-
-import com.projet.serviceImp.RegistrationRequest;
-import jakarta.servlet.Registration;
-
-public interface RegistrationService {
-    public String register( RegistrationRequest request);
-}
